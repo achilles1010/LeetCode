2403fc4b4deac38c7f99eb4ea28c7467a8e3b1ed
daixulin <daixulin@nash.work>
2020-06-04 17:15:47 +0800

    函数编程

---
+++ src/main/java/function/MyBiFunction.java
@@ -0,0 +1,15 @@
+package function;
+
+import java.util.function.BiFunction;
+
+/**
+ * @author daixulin
+ * @version : v1.0
+ * @date 2020-06-04 5:00 下午 周四
+ */
+public class MyBiFunction implements BiFunction<String, String, String> {
+    @Override
+    public String apply(String s, String s2) {
+        return s+";"+s2;
+    }
+}
