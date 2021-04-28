88469419fa45a84e5227610cf45575f9bcf8be36
daixulin <daixulin@nash.work>
2021-04-28 17:25:03 +0800

    minArray

---
+++ src/main/java/offer/alc/numWays.java
@@ -0,0 +1,24 @@
+package offer.alc;
+
+/**
+ * @author daixulin
+ * @version : v1.0
+ * @project :LeetCode
+ * @create 2021-04-28 下午4:03 周三
+ */
+public class numWays {
+    public static int func(int n) {
+        int a = 1, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+
+        return  a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(func(3));
+    }
+}
